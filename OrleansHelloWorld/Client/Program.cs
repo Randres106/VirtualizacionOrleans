63aef13d4ebfeffe13f8fbaae0246acf65edea66
Randres106 <60472929+Randres106@users.noreply.github.com>
2024-02-04 22:01:11 -0600

    Add files via upload

---
+++ OrleansHelloWorld/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using GrainInterfaces;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleansClient(client =>
+    {
+        client.UseLocalhostClustering();
+    })
+    .ConfigureLogging(logging => logging.AddConsole())
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+await host.StartAsync();
+
+IClusterClient client = host.Services.GetRequiredService<IClusterClient>();
+
+IHello friend = client.GetGrain<IHello>(0);
+string response = await friend.SayHello("Hi friend!");
+
+Console.WriteLine($"""
+    {response}
+
+    Press any key to exit...
+    """);
+
+Console.ReadKey();
+
+await host.StopAsync();
---
+++ OrleansHelloWorld/OrleansHelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering()
+            .ConfigureLogging(logging => logging.AddConsole());
+    })
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
